f6dd9ac75c099dd60cd3f2e545adbd8987a5a35b
Ahosan-1995 <ahsoanhabibshakil2@gmail.com>
2024-08-26 22:12:30 +0600

    add development  brach

---
+++ hi.js
@@ -0,0 +1,15 @@
+// git init
+
+/*
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Ahosan-1995/Practices.git
+git push -u origin main
+
+
+git remote add origin https://github.com/Ahosan-1995/Practices.git
+git branch -M main
+git push -u origin main
+
+*/
